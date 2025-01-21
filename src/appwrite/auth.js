990e568ec96ea70f0811ae2235e864e7bd31f533
Dhvanil-Pansuriya <dhvanilpansuriya2005@gmail.com>
2025-01-21 15:35:42 +0530

    first commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,62 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  async accountCreate({ name, email, password }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        name,
+        email,
+        password
+      );
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      console.log("AuthService :: accountCreate :: Error ", error);
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      console.log("AuthService :: login :: Error ", error);
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("AuthService :: getCurrentUser :: Error ", error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.log("AuthService :: logout :: Error ", error);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
---
+++ src/store/authSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  status: false,
+  userData: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (status, action) => {
+      status.status = true;
+      status.userData = action.payload.userData;
+    },
+
+    logout: (status) => {
+      status.status = false;
+      status.userData = null;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+
+export default authSlice.reducer;
